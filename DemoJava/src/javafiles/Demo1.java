d488c791cba727888c085ceba0d2f0f1ff145ee9
harshada1712 <harshada40patil@gmail.com>
2022-07-04 15:24:58 +0530

    first commit

---
+++ DemoJava/src/javafiles/Demo1.java
@@ -0,0 +1,12 @@
+package javafiles;
+
+public class Demo1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to GitHub");
+		
+
+	}
+
+}
---
+++ DemoJava/src/javafiles/Demo2.java
@@ -0,0 +1,14 @@
+package javafiles;
+
+public class Demo2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello GitHub");
+		int a=10;
+		int b=20;
+		int add=a+b;
+		 System.out.println("The Adition is : " +add);
+	}
+
+}
